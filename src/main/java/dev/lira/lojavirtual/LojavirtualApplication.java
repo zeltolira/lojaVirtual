9172bb71dc414366f33ea844c4f26ab7e4e4f7e9
zeltolira <zelto16@hotmail.com>
2025-01-22 05:59:57 -0300

    Altera application.properties para application.yml e configura o banco de dados

--- src/main/java/dev/lira/lojavirtual/LojavirtualApplication.java
+++ src/main/java/dev/lira/lojavirtual/LojavirtualApplication.java
@@ -2,9 +2,19 @@ package dev.lira.lojavirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping
 public class LojavirtualApplication {
+
+	@GetMapping
+	public String GetHomeTest(){
+		return "Minha Loja Virtual.com - Test OK!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojavirtualApplication.class, args);
