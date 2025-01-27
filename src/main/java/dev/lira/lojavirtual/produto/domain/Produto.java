b589bdea9abee21e9090fadb38640dc0b6319116
zeltolira <zelto16@hotmail.com>
2025-01-27 05:06:31 -0300

    implementa dominio de negocio

---
+++ src/main/java/dev/lira/lojavirtual/produto/domain/Produto.java
@@ -0,0 +1,32 @@
+package dev.lira.lojavirtual.produto.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@ToString
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "produto")
+public class Produto {
+    private UUID idPreco;
+    private String nome;
+    private BigDecimal preco;
+    @Enumerated(EnumType.STRING)
+    private TipoPromocao promocao;
+
+    public Produto(String nome, BigDecimal preco, TipoPromocao promocao) {
+        this.nome = nome;
+        this.preco = preco;
+        this.promocao = promocao;
+    }
+}
+
---
+++ src/main/java/dev/lira/lojavirtual/produto/domain/TipoPromocao.java
@@ -0,0 +1,20 @@
+package dev.lira.lojavirtual.produto.domain;
+
+import java.math.BigDecimal;
+
+public enum TipoPromocao {
+    LEVE_2_PAGUE_1, TRES_POR_DEZ;
+
+    public BigDecimal calcularDesconto(BigDecimal preco, int quantidade){
+        switch (this){
+            case LEVE_2_PAGUE_1:
+                return  preco.multiply(BigDecimal.valueOf(quantidade / 2));
+            case TRES_POR_DEZ:
+                int gruposDeTres = quantidade / 3;
+                int restantes = quantidade % 3;
+                return BigDecimal.valueOf(gruposDeTres * 10).add(preco.multiply(BigDecimal.valueOf(restantes)));
+            default:
+                return preco.multiply(BigDecimal.valueOf(quantidade));
+        }
+    }
+}
